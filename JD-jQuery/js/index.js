14f392aa532db314a3093fe3bf71250efb397b99
zhuangzhuangchen <1101145957@qq.com>
2019-01-05 09:49:30 +0800

    2019-1-5

--- JD-jQuery/js/index.js
+++ JD-jQuery/js/index.js
@@ -8,6 +8,31 @@
 		timer=setTimeout(function(){
 			$sideItem.slideToggle();
 		},50)
-	});
+	}); 
 	/*顶部导航菜单结束*/
+
+	/*搜索引擎开始*/
+
+	/*搜索引擎结束*/
+
+
+	/*轮播图开始*/
+	var $goodsListItem=$('.goods-list li');
+	var $goodsListLayer=$('.goods-list-layer');
+	$goodsListItem.hover(function(){
+		$goodsListLayer.toggleClass('goodsActive')
+	})
+	$goodsListLayer.hover(function(){
+		$goodsListLayer.toggleClass('goodsActive')
+	})
+	var oCourselList=$('.coursel-list')[0];
+	// console.log(oCourselList);
+	new Carousel({
+		id:"coursel-list",
+		aImg:['images/coursel1.jpg','images/coursel3.jpg','images/coursel4.jpg'],
+		width:1260,
+		height:448,
+		playDuration:3000
+	})
+	/*轮播图结束*/
 })(jQuery)
